12c6db64e5adc9a5733cd2e6a33daa46757c9d1a
Zhanghang <601104841@qq.com>
2016-03-30 22:29:06 +0800

    haha

--- main.go
+++ main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-
 	"fmt"
-	"io/ioutil"
+	"github.com/LearnEmprie/Martini/sample"
+	"net/http"
 )
 
-type Page struct  {
-	Title string
-	Body []byte
-
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := sample.LoadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename,p.Body,0600)
-}
-
-func loadPage(title string ) (*Page,error){
-	filename := title + ".txt"
-	body,err   := ioutil.ReadFile(filename)
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := sample.LoadPage(title)
 	if err != nil {
-		return nil, err
+		p = &sample.Page{Title: title}
 	}
-	return &Page{Title:title,Body:body},err
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+	"<form action=\"/save/%s\" method=\"POST\">"+
+	"<textarea name=\"body\">%s</textarea><br>"+
+	"<input type=\"submit\" value=\"Save\">"+
+	"</form>",
+		p.Title, p.Title, p.Body)
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.ListenAndServe(":8080", nil)
+
 }
