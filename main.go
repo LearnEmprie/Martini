a55b8d5511474370aa0cabb1b3946d1528708bb0
zhanghang <601104841@qq.com>
2016-04-07 17:05:54 +0800

    aa

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
+
 import (
+	"html/template"
 	"fmt"
 	"github.com/LearnEmprie/Martini/sample"
 	"net/http"
+	"io/ioutil"
 )
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := sample.LoadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+
+	httpGet()
+	t,err := template.ParseFiles("./view/index.html")
+	t.Execute(w,err)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,10 +30,25 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 		p.Title, p.Title, p.Body)
 }
 
+
 func main() {
 
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	http.ListenAndServe(":8080", nil)
 
+}
+
+
+func httpGet()  {
+	resp,err := http.Get("http://127.0.0.1:3000/string/key/1")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	body , err := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(string(body))
 }
